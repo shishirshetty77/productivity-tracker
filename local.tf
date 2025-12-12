c2ef12ce9ea267a2be5a269028acbd7643000bd4
shishir c shetty <shishirshetty217@gmail.com>
2025-12-13 02:03:22 +0530

    configured vpc and subnet nat and igw

---
+++ local.tf
@@ -0,0 +1,8 @@
+locals {
+  env         = "staging"
+  region      = "ap-south-1"
+  zone1       = "ap-south-1a"
+  zone2       = "ap-south-1b"
+  eks_name    = "aws-eks-demo"
+  eks_version = "1.31"
+}
