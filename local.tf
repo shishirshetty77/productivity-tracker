9e147b29831a516d2b96bb6f3201799478770b7f
shishir c shetty <shishirshetty217@gmail.com>
2026-01-01 07:33:45 +0530

    updated

--- local.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  env         = "staging"
-  region      = "ap-south-1"
-  zone1       = "ap-south-1a"
-  zone2       = "ap-south-1b"
-  eks_name    = "aws-eks-demo"
-  eks_version = "1.31"
-}
