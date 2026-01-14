6de8e16e2d97a223b375d51b6f13c7d6a575f995
shishir c shetty <shishirshetty217@gmail.com>
2026-01-15 00:23:22 +0530

    chore(vpc): Improve output descriptions

--- udemy/vpc/outputs.tf
+++ udemy/vpc/outputs.tf
@@ -1,14 +1,14 @@
 output "vpc_id" {
-  description = "VPC ID"
+  description = "The ID of the created VPC"
   value       = aws_vpc.main.id
 }
 
 output "private_subnet_ids" {
-  description = "Private subnet IDs"
+  description = "List of IDs of private subnets"
   value       = aws_subnet.private[*].id
 }
 
 output "public_subnet_ids" {
-  description = "Public subnet IDs"
+  description = "List of IDs of public subnets"
   value       = aws_subnet.public[*].id
 }
