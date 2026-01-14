9a996582af1686b69374095f865e8c6ace36ad57
shishir c shetty <shishirshetty217@gmail.com>
2026-01-15 00:24:37 +0530

    chore: Improve root module output descriptions

--- udemy/outputs.tf
+++ udemy/outputs.tf
@@ -1,15 +1,15 @@
 output "cluster_endpoint" {
-  description = "EKS cluster endpoint"
+  description = "The endpoint for your EKS Kubernetes API"
   value       = module.eks.cluster_endpoint
 }
 
 output "cluster_name" {
-  description = "EKS cluster name"
+  description = "The name of the EKS cluster"
   value       = module.eks.cluster_name
 }
 
 output "vpc_id" {
-  description = "VPC ID"
+  description = "The ID of the VPC"
   value       = module.vpc.vpc_id
 }
 
