c731e323672a9f984307a4ae0c07f4a4647de8b5
shishir c shetty <shishirshetty217@gmail.com>
2026-01-15 00:24:22 +0530

    chore(eks): Improve output descriptions

--- udemy/eks/outputs.tf
+++ udemy/eks/outputs.tf
@@ -1,9 +1,9 @@
 output "cluster_endpoint" {
-  description = "EKS cluster endpoint"
+  description = "The endpoint for your EKS Kubernetes API"
   value       = aws_eks_cluster.main.endpoint
 }
 
 output "cluster_name" {
-  description = "EKS cluster name"
+  description = "The name of the EKS cluster"
   value       = aws_eks_cluster.main.name
 }
